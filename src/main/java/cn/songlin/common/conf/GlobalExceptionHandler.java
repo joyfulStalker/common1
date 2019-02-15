21eee3dc06136a791c4305ef140319f9d005f244
lsl <liuslc@yonyou.com>
2019-02-15 16:43:57 +0800

    exception异常处理精简

--- src/main/java/cn/songlin/common/conf/GlobalExceptionHandler.java
+++ src/main/java/cn/songlin/common/conf/GlobalExceptionHandler.java
@@ -2,19 +2,24 @@ package cn.songlin.common.conf;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import cn.songlin.common.dto.base.BaseCode;
 import cn.songlin.common.dto.base.BaseResponseResult;
 import cn.songlin.common.exception.BizException;
 
-@ControllerAdvice
+/**
+ * 包含@ResponseBody 不加该注解，返回给浏览器的值是默认的
+ * 
+ * @author liusonglin
+ * @date 2019年2月15日
+ */
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
 	/**
-	 * @ResponseBody 不加该注解，返回给浏览器的值是默认的
 	 * @author liusonglin
 	 * @date 2018年10月31日
 	 * @param response
@@ -23,14 +28,12 @@ public class GlobalExceptionHandler {
 	 */
 
 	@ExceptionHandler(BizException.class)
-	@ResponseBody // 不加该注解，返回给浏览器的值是默认的
 	public BaseResponseResult baseExceptionHandler(HttpServletResponse response, BizException ex) {
 		ex.printStackTrace();
 		return new BaseResponseResult(ex.getMessage(), ex.getCode());
 	}
 
 	@ExceptionHandler(Exception.class)
-	@ResponseBody
 	public BaseResponseResult otherExceptionHandler(HttpServletResponse response, Exception ex) {
 		ex.printStackTrace();
 		return new BaseResponseResult(ex.getMessage(), BaseCode.ERROR_UNKNOWN);
